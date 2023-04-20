fb00dd8fc3065449f6d92b876e34be6e60cb9d50
ARBriterre <131387339+ARBriterre@users.noreply.github.com>
2023-04-20 23:16:50 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -20,41 +20,53 @@ def read_json_file(file_path):
 
 
 def load_data():
-    my_class_instance = read_json_file('data/bgp.json')
-    pass
+    flow = []
+    for line in open('data/flow.json', 'r'):
+        flow.append(json.loads(line))
+    x = []
+    for num, line in enumerate(flow):
+        src_port = line.get("src_port", None)
+        dst_port = line.get("dst_port", None)
+        dst4_addr = line.get("dst4_addr", None)
+        temp = [src_port, dst_port, dst4_addr]
+        if None not in temp:
+            x.append(temp)
 
+    return x
 
+print(len(load_data()))
 
-X_train, X_test, y_train, y_test = load_data()
-
-
-def preprocess_data(X_train, X_test):
-    pass
-
-
-X_train = preprocess_data(X_train)
-X_test = preprocess_data(X_test)
-
-# Train the DBN
-dbn = BernoulliRBM(n_components=100, learning_rate=0.01, n_iter=10, verbose=0)
-dbn.fit(X_train)
-
-# Extract features
-X_train_features = dbn.transform(X_train)
-X_test_features = dbn.transform(X_test)
-
-# Train the SVM
-svm = SVC(kernel='rbf', C=1, gamma=0.1)
-svm.fit(X_train_features, y_train)
-
-# Create a pipeline for DBN-SVM
-dbn_svm = Pipeline(steps=[('dbn', dbn), ('svm', svm)])
-
-# Evaluate the model
-y_pred = dbn_svm.predict(X_test)
-accuracy = accuracy_score(y_test, y_pred)
-precision, recall, f1_score, _ = precision_recall_fscore_support(y_test, y_pred)
-
-print('Accuracy:', accuracy)
-print('Precision:', precision)
-xprint('F1-score:', f1_score)
+#
+# X_train, X_test, y_train, y_test = load_data()
+#
+#
+# def preprocess_data(X_train, X_test):
+#     pass
+#
+#
+# X_train = preprocess_data(X_train)
+# X_test = preprocess_data(X_test)
+#
+# # Train the DBN
+# dbn = BernoulliRBM(n_components=100, learning_rate=0.01, n_iter=10, verbose=0)
+# dbn.fit(X_train)
+#
+# # Extract features
+# X_train_features = dbn.transform(X_train)
+# X_test_features = dbn.transform(X_test)
+#
+# # Train the SVM
+# svm = SVC(kernel='rbf', C=1, gamma=0.1)
+# svm.fit(X_train_features, y_train)
+#
+# # Create a pipeline for DBN-SVM
+# dbn_svm = Pipeline(steps=[('dbn', dbn), ('svm', svm)])
+#
+# # Evaluate the model
+# y_pred = dbn_svm.predict(X_test)
+# accuracy = accuracy_score(y_test, y_pred)
+# precision, recall, f1_score, _ = precision_recall_fscore_support(y_test, y_pred)
+#
+# print('Accuracy:', accuracy)
+# print('Precision:', precision)
+# xprint('F1-score:', f1_score)
